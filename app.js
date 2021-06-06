14633602509425e97e82d3dcdb3a5d799b5b511e
Komarti Venkata Teja <49075779+venkatateja111@users.noreply.github.com>
2021-06-06 13:30:03 +0530

    initial

--- app.js
+++ app.js
@@ -235,4 +235,18 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
+if (process.env.NODE_ENV === 'production') {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, 'client/build')));
+// Handle React routing, return all requests to React app
+  app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  });
+}
+
+const PORT = process.env.PORT || 9000;
+app.listen(PORT)
+
+
+
 module.exports = app;
